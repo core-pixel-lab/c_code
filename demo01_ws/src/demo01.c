bed8eee44f20d57c1bd9e5d002071e59a5fcd755
Holden Hsu <holden_hsu@163.com>
2026-02-03 21:23:13 -0800

    run succeed

--- demo01_ws/src/demo01.c
+++ demo01_ws/src/demo01.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include "../include/demo01.h"
+
+test demo01;
 
 int main(int argc, char const *argv[])
 {
-    printf("hello,world!");
+    demo01.id = 1;
+    demo01.num = 0.5;
+    printf("Hello,world!\n");
+    printf("The id of demo01 is %d,\n",demo01.id);
+    printf("the num of demo01 is %f.\n",demo01.num);
 
     return 0;
 }
