8d45835dd7ed2077e430462e9095f3a709aaceea
Holden Hsu <holden_hsu@163.com>
2026-02-03 21:48:28 -0800

    cmake

---
+++ demo01_ws/build/CMakeFiles/demo01.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo01.dir/src/demo01.c.o"
+  "CMakeFiles/demo01.dir/src/demo01.c.o.d"
+  "demo01"
+  "demo01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/demo01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
